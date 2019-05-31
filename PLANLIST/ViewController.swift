14b0ba5dea0a3bf2104ead23382293ee228f9b8d
和城信行 <washiro@wajoushingyou-no-macbook-air.local>
2019-05-31 19:57:00 +0900

    Initial Commit

---
+++ PLANLIST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PLANLIST
+//
+//  Created by 和城信行 on 2019/05/31.
+//  Copyright © 2019年 和城信行. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
